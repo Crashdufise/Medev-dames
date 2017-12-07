07c8e9b465696ef3bdea8b01d7b5478d904d59ef
Louis et Thomas <33541134+JeleoRV@users.noreply.github.com>
2017-12-07 12:07:13 +0100

    Gestion de l'affichage, début de la gestion du déplacement
    Le cas de la dame ne sera pour le moment pas pris en compte

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 
 
 vector<vector<checker>> initialisation() {
-	vector<vector<checker>> _damier;
+	vector<vector<checker>> _damier; // Cet objet sera rempli selon les règles classiques du jeu de dames
 	for (int i=0; i<10; i++) {
-		vector<checker> ligne;
+		vector<checker> ligne; // On remplit ligne par ligne
 		for (int j=0; j<10; j++) {
 			if((i+j)%2 == 0) 
 			{
@@ -40,24 +40,56 @@ vector<vector<checker>> initialisation() {
 	return _damier;
 }
 
+void affichage(vector<vector<checker>> damier) {
+	cout<<endl; // Sépare la grille des résultats des tests
+	for(int i=0; i < 10; i++) {
+		cout<<"|"; // Ajoute la colonne de gauche
+		for(int j=0; j < 10; j++) {
+			cout<<((damier[9-i][j].getIsNull() == true)?" ":((damier[9-i][j].getColor() == true)?"b":"n"))<<"|"; // Affiche les pions par colonne
+		}
+		cout<<endl;
+	}
+}
 
-TEST(initialisation, PlaceAuxBonsEndroits) {
+TEST(initialisation, PlaceBlanc) { // Vérifie si l'initialisation place bien un pion blanc en [0,0]
 	EXPECT_EQ(true, initialisation()[0][0].getColor());
+}
+TEST(initialisation, PlaceNoir) { // Vérifie si l'initialisation place bien un pion noir en [9,9]
+	EXPECT_EQ(false, initialisation()[9][9].getColor());
+}
+TEST(initialisation, PlaceNull) { // Vérifie si l'initialisation place bien des pions Null en [5,5], en [0,1], et en [8,9]
+	EXPECT_EQ(true, initialisation()[5][5].getIsNull());
+	EXPECT_EQ(true, initialisation()[0][1].getIsNull());
+	EXPECT_EQ(true, initialisation()[8][9].getIsNull());
 }
 
 int main(int argc, char*argv[]) {
+	// Permet d'exécuter les tests Google Test
 	::testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
 
-	vector<vector<checker>> damier;
+	vector<vector<checker>> damier; // Grille du jeu de dames
 
-	damier = initialisation();
+	damier = initialisation(); // Remplissage de la grille
 
 
-	bool gameIsNOTOVER = true;
+	bool gameIsOVER = false; // Permettra d'arrêter le jeu
 
-	while(gameIsNOTOVER) {
-		gameIsNOTOVER = false;
+	affichage(damier);
+
+	while(!gameIsOVER) { // Exécution du jeu de dame à deux joueurs humains
+		int _posX; 
+		int _posY;
+		// Sélection du pion qui sera joué
+		cout << endl << "Position X du pion choisi : "; 
+		cin >> _posX;
+		cout << endl << "Position Y du pion choisi : ";
+		cin >> _posY;
+
+		// Déplacement du pion
+
+
+		gameIsOVER = true;
 
 	}
 }
